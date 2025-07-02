6a9579d9b416e4f1a50929c9f15f6dccb328a98b
Nfoua Eugene <nfouaeugene953@gmail.com>
2025-07-02 21:38:21 +0100

    connect cloud database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,4 @@
-
-import { Pool, PoolClient } from 'pg';
+ import { Pool, PoolClient } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -28,4 +27,27 @@ pool.connect(
   }
 );
 
-export default pool
+export default pool;
+
+
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: false, 
+});
+
+// Test the connection
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('Failed to connect to Supabase PostgreSQL:', err.stack);
+  } else {
+    console.log('Connected to Supabase PostgreSQL successfully!');
+    release();
+  }
+});
+
+export default pool;
