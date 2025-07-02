6473087d1c3ec2cb23e205f76725b925caef2128
Nfoua Eugene <nfouaeugene953@gmail.com>
2025-07-02 21:47:05 +0100

    connect cloud database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,33 +1,3 @@
- import { Pool, PoolClient } from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: 'eugene953',
-  database: 'MentalHealhSupportBot',
-});
-
-// Test the connection
-pool.connect(
-  (
-    err: Error | undefined,
-    client: PoolClient | undefined,
-    release: (() => void) | undefined
-  ) => {
-    if (err) {
-      console.error('No Connection to PostgreSQL Database', err.stack);
-    } else if (client && release) {
-      console.log('PostgreSQL Database Connected Successfully');
-      release();
-    }
-  }
-);
-
-export default pool;
 
 
 import { Pool } from 'pg';
